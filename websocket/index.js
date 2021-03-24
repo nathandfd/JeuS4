358a06cdd514d3383d6b753a627271c064aec7e0
Nathan Dufaud <nathan.dufaud@gmail.com>
2021-03-24 23:55:38 +0100

    'Update'

--- websocket/index.js
+++ websocket/index.js
@@ -9,10 +9,11 @@ const server = require('https').Server({
 },app)
 const io = require('socket.io')(server)
 
-app.use(cors({
-    origin: 'https://nathandfd.fr'
-}));
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.get('/',(req,res)=>{
     res.end('nik tes morts')
     io.emit('foo','bar')
