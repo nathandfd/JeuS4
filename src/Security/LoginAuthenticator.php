22a78ab170a7fb510175df6dd61526f843ae1e69
Nathan Dufaud <nathan.dufaud@gmail.com>
2021-03-25 21:00:38 +0100

    'Update'

--- src/Security/LoginAuthenticator.php
+++ src/Security/LoginAuthenticator.php
@@ -71,7 +71,7 @@ class LoginAuthenticator extends AbstractFormLoginAuthenticator implements Passw
 
         if (!$user) {
             // fail authentication with a custom error
-            throw new CustomUserMessageAuthenticationException('Email could not be found.');
+            throw new CustomUserMessageAuthenticationException('Email could not be found. Et ça c\'est con');
         }
 
         return $user;
@@ -79,7 +79,15 @@ class LoginAuthenticator extends AbstractFormLoginAuthenticator implements Passw
 
     public function checkCredentials($credentials, UserInterface $user)
     {
-        return $this->passwordEncoder->isPasswordValid($user, $credentials['password']);
+        //return $this->passwordEncoder->isPasswordValid($user, $credentials['password']);
+        if ($this->passwordEncoder->isPasswordValid($user, $credentials['password'])){
+            if ($user->isVerified()){
+                return true;
+            }
+            else{
+                throw new CustomUserMessageAuthenticationException('nan nan vérifie ton mail fdp');
+            }
+        }
     }
 
     /**
