eb180ae4c9740e9533e54bf5a66334fc5e57b543
Nathan Dufaud <nathan.dufaud@gmail.com>
2021-03-25 11:14:39 +0100

    'Update'

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -15,7 +15,6 @@ class DefaultController extends AbstractController
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'DefaultController',
-            'user_id'=>$this->getUser()->getId()
         ]);
     }
 }
