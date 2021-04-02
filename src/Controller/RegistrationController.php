45712ec5d717de695a34aa3ed3c0eb6982ea9931
Nathan Dufaud <nathan.dufaud@gmail.com>
2021-04-02 20:29:43 +0200

    Captcha added on registration form

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -38,9 +38,11 @@ class RegistrationController extends AbstractController
             $user->setPassword(
                 $passwordEncoder->encodePassword(
                     $user,
-                    $form->get('plainPassword')->getData()
+                    $form->get('password')->getData()
                 )
             );
+
+            $user->setBirthday($form->get('birthday')->getData());
 
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($user);
